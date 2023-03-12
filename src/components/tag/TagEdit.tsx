f534b129893dacfc4a7b4698f6695885acf55372
my <1119312377@qq.com>
2023-03-12 17:17:22 +0800

    删除标签

--- src/components/tag/TagEdit.tsx
+++ src/components/tag/TagEdit.tsx
@@ -4,13 +4,30 @@ import { Button } from "../../shared/Button";
 import { TagForm } from "./TagForm";
 import s from './Tag.module.scss';
 import { BackIcon } from "../../shared/BackIcon";
-import { useRoute } from "vue-router";
+import { useRoute, useRouter } from "vue-router";
+import { http } from "../../shared/Http";
+import { Dialog } from "vant";
 export const TagEdit = defineComponent({
     setup: (props, context) => {
         const route = useRoute()
         const numberId = parseInt(route.params.id!.toString())
         if (Number.isNaN(numberId)) {
             return () => { <div>ID不存在</div> }
+        }
+        const onError = () => {
+            Dialog.alert({
+                title: '提示',
+                message: '删除失败'
+            })
+        }
+        const router = useRouter()
+        const onDelete = async (options?: { withItems: boolean }) => {
+            await http.delete(`/tags/${numberId}`, { withItems: options?.withItems ? 'true' : 'false' })
+            await Dialog.confirm({
+                title: '确认',
+                message: '你真的要删除吗？'
+            }).catch(onError)
+            router.back()
         }
         return () => (
             <MainLayout>{{
@@ -19,8 +36,8 @@ export const TagEdit = defineComponent({
                 default: () => (<>
                     <TagForm id={numberId} />
                     <div class={s.actions}>
-                        <Button level="danger" class={s.removeTags} onClick={() => ({})} > 删除标签</Button>
-                        <Button level="danger" class={s.removeTagsAndItems} onClick={() => ({})} >删除标签和记账</Button>
+                        <Button level="danger" class={s.removeTags} onClick={() => onDelete()} > 删除标签</Button>
+                        <Button level="danger" class={s.removeTagsAndItems} onClick={() => onDelete({ withItems: true })} >删除标签和记账</Button>
                     </div>
 
                 </>
